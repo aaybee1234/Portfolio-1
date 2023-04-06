4fab4018830d97eda650aaad2981e05bdeaaba41
abubakr <43236536+aaybee1234@users.noreply.github.com>
2023-04-06 15:06:18 +0500

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> ABUBAKAR ARSHAD</strong>
+                <strong className="main-name"> Abubakar M Arshad</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
